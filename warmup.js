a91e842da5d17c5b090d1f491b5a40521e969cdc
oussamanaffati1 <oussamanaffati270@gmail.com>
2021-07-10 14:22:11 +0100

    msg

---
+++ warmup.js
@@ -0,0 +1,43 @@
+  
+// 1-using + operator combine your partner first and last name .
+
+function combine(a,b) {
+    return (a,b)
+    a=oussama
+    b=naffati
+  console.log(a+""+ b)
+  // a,b=oussama naffati 
+  
+}
+
+
+// 2-find if the number 13 is a multiple of 3 or not.
+function MultipleOF3(a) {
+   return (a)
+   a=13
+   console.log (13%3)
+  // a=1 13 is not mutiple of 3  
+
+
+
+
+  // 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
+var ages = a [13, 14, 13, 15, 16, 17, 19, 13, 16, 15];
+      b=(13+14+13+15+16+17+19+13+16+15)
+       (a/b)
+       console.log(15 )
+
+      // Divide the total number of ages by the number of people in the group. In the example, 151 divided by 10 
+
+
+      //----------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+       //  -warmup2
+
+
+       
